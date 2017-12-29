1cfa255fa6163d6c252c8f325748f2da2b5d166a
SithijaLakshan <sithikaru@gmail.com>
2017-12-29 08:27:47 +0530

    commit

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,8 +4,8 @@
             <div class="flex-center position-ref full-height">
 
             <div class="content">
-                <div class="title m-b-md ani" >
-                    Contact the <strong>{{ config('app.name', 'Laravel') }}</strong>
+                <div class="title m-b-md animated zoomIn" >
+                    Contact the <strong>{{ config('app.name', 'iceCream') }}</strong>
                 </div>
             </div>
         </div>
